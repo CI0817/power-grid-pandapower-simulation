6613281a320ebc290d573932fdf131100026734d
Chhay Irseng <cirs0001@student.monash.edu>
2025-07-04 19:57:14 +1000

    feat: Parameterize complex network creation

--- complex_network.py
+++ complex_network.py
@@ -2,12 +2,25 @@ import pandapower as pp
 from network_analysis import run_diagnosis, plot_network
 from tabulate import tabulate
 
-def create_complex_network():
+def create_complex_network(
+    bus_voltage_pu=1.02,
+    residential_load_p_mw=0.15,
+    residential_load_q_mvar=0.05,
+    commercial_load_p_mw=0.25,
+    commercial_load_q_mvar=0.1,
+    solar_p_mw=0.05,
+    main_line_length_km=50,
+    feeder_section1_length_km=2.5,
+    feeder_section2_length_km=1.5,
+    trafo_terminal_station_sn_mva=100,
+    trafo_zone_substation_sn_mva=25,
+    network_name="Simplified Melbourne Grid Section"
+):
     """
     Creates a complex power grid network using pandapower.
     """
     # Create an Empty Network
-    net = pp.create_empty_network(name="Simplified Melbourne Grid Section")
+    net = pp.create_empty_network(name=network_name)
 
     # Define Standard Component Types
     # Transmission Line Type (220kV)
@@ -21,14 +34,16 @@ def create_complex_network():
     }, element='line')
 
     # Terminal Station Transformer (220kV/66kV)
-    pp.create_std_type(net, name="100 MVA 220/66 kV", data={
-        "sn_mva": 100, "vn_hv_kv": 220, "vn_lv_kv": 66, "vk_percent": 12.0,
+    terminal_trafo_name = f"{trafo_terminal_station_sn_mva} MVA 220/66 kV"
+    pp.create_std_type(net, name=terminal_trafo_name, data={
+        "sn_mva": trafo_terminal_station_sn_mva, "vn_hv_kv": 220, "vn_lv_kv": 66, "vk_percent": 12.0,
         "vkr_percent": 0.26, "pfe_kw": 50, "i0_percent": 0.06, "shift_degree": 0
     }, element="trafo")
 
     # Zone Substation Transformer (66kV/11kV)
-    pp.create_std_type(net, name="25 MVA 66/11 kV", data={
-        "sn_mva": 25, "vn_hv_kv": 66, "vn_lv_kv": 11, "vk_percent": 10.0,
+    zone_trafo_name = f"{trafo_zone_substation_sn_mva} MVA 66/11 kV"
+    pp.create_std_type(net, name=zone_trafo_name, data={
+        "sn_mva": trafo_zone_substation_sn_mva, "vn_hv_kv": 66, "vn_lv_kv": 11, "vk_percent": 10.0,
         "vkr_percent": 0.3, "pfe_kw": 20, "i0_percent": 0.07, "shift_degree": 0
     }, element="trafo")
 
@@ -46,29 +61,29 @@ def create_complex_network():
 
     # Build the Transmission and Sub-Transmission Layer (220kV & 66kV)
     bus_220kv_infeed = pp.create_bus(net, vn_kv=220, name="220kV Infeed (NEM)")
-    pp.create_ext_grid(net, bus=bus_220kv_infeed, vm_pu=1.02, name="External Grid")
+    pp.create_ext_grid(net, bus=bus_220kv_infeed, vm_pu=bus_voltage_pu, name="External Grid")
 
     bus_220kv_terminal = pp.create_bus(net, vn_kv=220, name="220kV Terminal Station")
     bus_66kv_subtrans = pp.create_bus(net, vn_kv=66, name="66kV Sub-transmission")
 
-    pp.create_line(net, from_bus=bus_220kv_infeed, to_bus=bus_220kv_terminal, length_km=50,
+    pp.create_line(net, from_bus=bus_220kv_infeed, to_bus=bus_220kv_terminal, length_km=main_line_length_km,
                   std_type="220kV_line", name="Main Transmission Line")
 
     pp.create_transformer(net, hv_bus=bus_220kv_terminal, lv_bus=bus_66kv_subtrans,
-                         std_type="100 MVA 220/66 kV", name="Terminal Station Trafo")
+                         std_type=terminal_trafo_name, name="Terminal Station Trafo")
 
     # Build the Medium Voltage Distribution Layer (11kV)
     bus_11kv_dist = pp.create_bus(net, vn_kv=11, name="11kV Distribution Feeder Start")
 
     pp.create_transformer(net, hv_bus=bus_66kv_subtrans, lv_bus=bus_11kv_dist,
-                         std_type="25 MVA 66/11 kV", name="Zone Substation Trafo")
+                         std_type=zone_trafo_name, name="Zone Substation Trafo")
 
     bus_11kv_mid = pp.create_bus(net, vn_kv=11, name="11kV Feeder Midpoint")
     bus_11kv_end = pp.create_bus(net, vn_kv=11, name="11kV Feeder End")
 
-    pp.create_line(net, from_bus=bus_11kv_dist, to_bus=bus_11kv_mid, length_km=2.5,
+    pp.create_line(net, from_bus=bus_11kv_dist, to_bus=bus_11kv_mid, length_km=feeder_section1_length_km,
                   std_type="11kV_line", name="Feeder Section 1")
-    pp.create_line(net, from_bus=bus_11kv_mid, to_bus=bus_11kv_end, length_km=1.5,
+    pp.create_line(net, from_bus=bus_11kv_mid, to_bus=bus_11kv_end, length_km=feeder_section2_length_km,
                   std_type="11kV_line", name="Feeder Section 2")
 
     # Build the Low Voltage (LV) Layer and connect Customers (0.4kV)
@@ -80,10 +95,10 @@ def create_complex_network():
     pp.create_transformer(net, hv_bus=bus_11kv_end, lv_bus=bus_lv_2,
                          std_type="0.63 MVA 11/0.4 kV", name="Pad-mount Trafo 2")
 
-    pp.create_load(net, bus=bus_lv_1, p_mw=0.15, q_mvar=0.05, name="Residential Load")
-    pp.create_load(net, bus=bus_lv_2, p_mw=0.25, q_mvar=0.1, name="Commercial Load")
+    pp.create_load(net, bus=bus_lv_1, p_mw=residential_load_p_mw, q_mvar=residential_load_q_mvar, name="Residential Load")
+    pp.create_load(net, bus=bus_lv_2, p_mw=commercial_load_p_mw, q_mvar=commercial_load_q_mvar, name="Commercial Load")
 
-    pp.create_sgen(net, bus=bus_lv_1, p_mw=0.05, q_mvar=0, name="Rooftop Solar")
+    pp.create_sgen(net, bus=bus_lv_1, p_mw=solar_p_mw, q_mvar=0, name="Rooftop Solar")
     
     return net
 
