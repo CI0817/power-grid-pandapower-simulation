e71707bcf88be57f9cf420fd43ec6edc6252fc47
Chhay Irseng <cirs0001@student.monash.edu>
2025-07-06 23:35:09 +1000

    refactor: Remove complex network simulation option from main script as it is a redundant script and can be recreated using the data_driven_network script

--- src/main.py
+++ src/main.py
@@ -10,23 +10,18 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from src.network_analysis import run_diagnosis, run_contingency_analysis
 from src.simple_network import create_simple_network
-from src.complex_network import create_complex_network
 from src.data_driven_network import create_data_driven_network
 from src.case14 import create_case14_network
 
 def main():
     parser = argparse.ArgumentParser(description="Power Grid Simulation Tool")
-    parser.add_argument('simulation', choices=['simple', 'complex', 'data_driven', 'case14', 'contingency'], help='The simulation to run.')
+    parser.add_argument('simulation', choices=['simple', 'data_driven', 'case14', 'contingency'], help='The simulation to run.')
 
     args = parser.parse_args()
 
     if args.simulation == 'simple':
         net = create_simple_network()
         run_diagnosis(net, 'Simple Network')
-        simple_plot(net, plot_loads=True, plot_sgens=True, plot_gens=True)
-    elif args.simulation == 'complex':
-        net = create_complex_network()
-        run_diagnosis(net, 'Complex Network')
         simple_plot(net, plot_loads=True, plot_sgens=True, plot_gens=True)
     elif args.simulation == 'data_driven':
         with open('data/network_config.json', 'r') as f:
